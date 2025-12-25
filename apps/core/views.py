5d7601bdee3038d0a2b055d1837df613599eaeed
Yuriy <myskkk@yandex.ru>
2025-12-25 10:28:59 +0500

    edit # apps/core/views.py

--- apps/core/views.py
+++ apps/core/views.py
@@ -654,14 +654,14 @@ from django.views.decorators.csrf import requires_csrf_token
 def csrf_failure(request, reason=""):
     return render(request, '403_csrf.html', status=403)
 
-def handler404(request, exception):
-    return render(request, '404.html', status=404)
+# def handler404(request, exception):
+#     return render(request, '404.html', status=404)
 
-def handler500(request):
-    return render(request, '500.html', status=500)
+# def handler500(request):
+#     return render(request, '500.html', status=500)
 
-def handler403(request, exception):
-    return render(request, '403.html', status=403)
+# def handler403(request, exception):
+#     return render(request, '403.html', status=403)
 
-def handler400(request, exception):
-    return render(request, '400.html', status=400)
+# def handler400(request, exception):
+#     return render(request, '400.html', status=400)
